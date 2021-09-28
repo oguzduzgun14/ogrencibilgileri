ed8e3a231cb0075c84b134a4c06b81ba73b0ee3c
oguzduzgun14 <oguzduzgun1414@gmail.com>
2021-09-28 14:04:24 +0300

    c gxcf

---
+++ src/ogrencibilgileri/Main.java
@@ -0,0 +1,34 @@
+package ogrencibilgileri;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ogrenci bilgileri");
+		System.out.print("isim ");
+		System.out.print(" ");
+		System.out.println("yaþar oðuz");
+		System.out.print("soyisim :");
+		System.out.println("düzgün");
+		System.out.println("yaþ :"+15);
+		System.out.print("okulu :");
+		System.out.println("izzet baysal mesleki ve teknik anadolu lisesi");
+		System.out.print("bölüm :");
+		System.out.println("biliþim teknolejileri");
+		
+		
+		
+		
+		
+		
+		
+			
+				
+				
+				
+				
+		
+
+	}
+
+}
